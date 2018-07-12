aa3b09dcd21b3d148d9723abdfd0fe2242a4f341
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-12 15:33:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,4 +5,11 @@ class App < Sinatra::Base
   get do '/name'
     "My name is Kayla"
   end
+
+  get do '/hometown'
+    "My hometown is Brownsville"
+  end
+
+  get do '/favorite-song'
+    "My favorite song is This is Me"
 end
